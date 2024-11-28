2177f70c165618bf49a45cc0c01fa96c1b012fd6
Matheus <matheus081105@gmail.com>
2024-11-28 19:34:16 -0300

    fix: Correção de um retorno da api para teste de novos programadores

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -3,5 +3,8 @@ import { getUserById } from '../../lib/queries';
 export default async function handler(req, res) {
     const { id } = req.query;
     const user = await getUserById(id);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
     res.status(200).json(user);
 }
